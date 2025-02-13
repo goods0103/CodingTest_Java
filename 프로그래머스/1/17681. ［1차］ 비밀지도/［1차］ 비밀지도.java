cdff487f9f618c5d5d347bbdaba4b9a950df103b
jeyoung <goods0103@naver.com>
2025-02-13 15:17:04 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.72 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,8 +2,9 @@ class Solution {
     public String[] solution(int n, int [] arr1, int [] arr2) {
         String[] answer = new String[n];
         for(int i=0; i<arr1.length; i++){
+            int temp = arr1[i] | arr2[i];
             //2진수 변환
-            String s = Integer.toBinaryString(arr1[i]|arr2[i]);
+            String s = String.format("%"+n+"s", Integer.toBinaryString(arr1[i]|arr2[i]));
             // String s3 = String.format("%"+n+"d",Long.parseLong(Integer.toBinaryString(arr1[i])));
             System.out.println("arr1 : "+arr1[i]);
             System.out.println("arr2 : "+arr2[i]);
@@ -13,9 +14,9 @@ class Solution {
             // answer[i] = Integer.toString((arr2[i]+arr1[i])).replace("0", " ");
             System.out.println("ans length: "+answer[i].length());
 
-            while(answer[i].length()<n){
-                answer[i]=" "+answer[i];
-            }
+            // while(answer[i].length()<6){
+            //     answer[i]=" "+answer[i];
+            // }
             System.out.println("ans length: "+answer[i].length());
 
             System.out.println("ans : "+answer[i]);
