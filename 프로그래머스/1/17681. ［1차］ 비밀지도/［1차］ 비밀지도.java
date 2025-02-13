a7eb1950a13f0447817e04843bf06cc5375532ec
jeyoung <goods0103@naver.com>
2025-02-13 14:43:27 +0900

    [level 1] Title: [1차] 비밀지도, Time: 20.77 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] solution(int n, int [] arr1, int [] arr2) {
+        String[] answer = new String[n];
+        for(int i=0; i<arr1.length; i++){
+            //2진수 변환
+            String s = Integer.toBinaryString(arr1[i]|arr2[i]);
+            // String s3 = String.format("%"+n+"d",Long.parseLong(Integer.toBinaryString(arr1[i])));
+            System.out.println("arr1 : "+arr1[i]);
+            System.out.println("arr2 : "+arr2[i]);
+            // System.out.println(arr2[i]+arr1[i]);
+            answer[i] = s.replace("0", " ");
+            answer[i] = answer[i].replace("1", "#");
+            // answer[i] = Integer.toString((arr2[i]+arr1[i])).replace("0", " ");
+            System.out.println("ans length: "+answer[i].length());
+
+            while(answer[i].length()<n){
+                answer[i]=" "+answer[i];
+            }
+            System.out.println("ans length: "+answer[i].length());
+
+            System.out.println("ans : "+answer[i]);
+        }
+        return answer;
+    }
+}
